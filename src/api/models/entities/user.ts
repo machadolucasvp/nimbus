e7cb45ae1fa97316b6437c992b753ee26a5ea754
Lucas Machado <machadolucasvp@gmail.com>
2020-08-01 23:49:13 -0300

    feat(user): add user model with role enumerator

---
+++ src/api/models/entities/user.ts
@@ -0,0 +1,41 @@
+import { Entity, Column, PrimaryColumn, UpdateDateColumn, CreateDateColumn } from "typeorm";
+import { IsEmail, Length, IsBoolean } from 'class-validator';
+
+import { Role } from '../enums'
+
+@Entity()
+export class User {
+
+  @PrimaryColumn()
+  id!: number;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.GUEST
+  })
+  role!: Role
+
+  @Column()
+  @Length(4, 40)
+  name!: string;
+
+  @IsEmail()
+  @Length(10, 64)
+  email!: string;
+
+  @Column()
+  @Length(11, 14)
+  cpfOrCnpj!: string;
+
+  @Column({ default: true })
+  @IsBoolean()
+  isActive!: boolean;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+}
---
+++ src/api/models/enums/role.ts
@@ -0,0 +1,4 @@
+export enum Role {
+  ADMIN = "admin",
+  GUEST = "guest"
+}
