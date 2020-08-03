1eb2157df5e7fdf57ca11e10ffbf190cda31e57d
Lucas Machado <machadolucasvp@gmail.com>
2020-08-02 23:04:08 -0300

    chore: add jsonwebtoken, brcrypt and adjust app for boot with orm connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
+import { createConnection } from 'typeorm';
 
 import { Router } from './routes';
-const app = new Koa();
 
-app.use(bodyParser())
-app.use(logger())
+(async function bootstrap() {
+  const app = new Koa();
 
-app.use(Router.routes())
+  app.use(bodyParser())
+  app.use(logger())
+
+  try {
+    await createConnection();
+  } catch (err) {
+    console.log({ error: { ...err } })
+  }
+
+  app.use(Router.routes())
+})()
