f6e0a83f26af2cecb1c42b261dbb14ed964a02fa
Lucas Machado <machadolucasvp@gmail.com>
2020-08-05 00:26:44 -0300

    chore: add amqplib

--- src/api/infra/typeorm/index.ts
+++ src/api/infra/typeorm/index.ts
@@ -3,7 +3,6 @@ import { createConnection } from 'typeorm';
 (async function bootTypeORM() {
   try {
     await createConnection();
-    console.log('criei')
   } catch (err) {
     console.log({ error: { ...err } })
   }
