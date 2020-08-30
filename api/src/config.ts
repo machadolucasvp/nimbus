f71c513e9b243c69027dfafe3244c041402979b8
Lucas Machado <machadolucasvp@gmail.com>
2020-08-30 19:18:42 -0300

    chore(Dockerfile): add dockerfile with build and prod stages

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_PORT = process.env.JWT_SECRET || 8000
+export const SERVER_PORT = process.env.JWT_SECRET || 8001
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'somethinghashed'
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "24 hours"
