cfbbae2828ff334df0dd33aef24eb360d685694f
Lucas Machado <machadolucasvp@gmail.com>
2020-09-01 23:11:44 -0300

    fix(worker): fix rabbitmq conn error, making bootRabbitMq sync with workerPool

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,14 +1,17 @@
 import { WorkerPool } from './pool';
+import { bootRabbitMQ } from './broker';
 
 import { WORKERS_AMOUNT } from './config';
 
 const composeIfExists = (value: any, func?: Function) => func ? func(value) : func;
 
-(function initWorkersPool(callback?: Function) {
+(async function initWorkersPool(callback?: Function) {
   try {
+    await bootRabbitMQ()
     const workerPool = new WorkerPool(WORKERS_AMOUNT);
 
     workerPool.init();
+
     composeIfExists(workerPool.size, callback);
   } catch (err) {
     console.log({ error: { ...err } })
