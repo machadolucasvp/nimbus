172198e08bb21c9a66e2c4041ba0ccf6c86429a3
Lucas Machado <machadolucasvp@gmail.com>
2020-09-14 23:32:18 -0300

    chore(nimbus-rabbit): add rabbitmq docker compose specification

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,6 +3,6 @@ export const SERVER_PORT = process.env.SERVER_PORT || 8001
 export const JWT_SECRET = process.env.JWT_SECRET || "secret"
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "24 hours"
 
-export const AMQP_URI = process.env.AMQP_URI || "amqp://localhost:15672"
+export const AMQP_URI = process.env.AMQP_URI || `amqp://${process.env.AMQP_HOST}:${process.env.AMQP_PORT}`
 export const RABBIT_PREFETCH = Number(process.env.RABBIT_PREFETCH) || 1
 export const RABBIT_QUEUES = process.env.RABBIT_QUEUES?.split(',') || ["mailer"]
--- worker/src/config.ts
+++ worker/src/config.ts
@@ -1,4 +1,4 @@
-export const AMQP_URI = process.env.AMQP_URI || "amqp://localhost:15672"
+export const AMQP_URI = process.env.AMQP_URI || `amqp://${process.env.AMQP_HOST}:${process.env.AMQP_PORT}`
 export const RABBIT_PREFETCH = Number(process.env.RABBIT_PREFETCH) || 1
 export const RABBIT_QUEUES = process.env.RABBIT_QUEUES?.split(",") || ['mailer']
 
